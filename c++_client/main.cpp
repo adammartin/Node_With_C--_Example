1efe7ca566a8cb46b47609cd0d0638b888621d03
adammartin <ebondark.od@gmail.com>
2013-08-21 14:53:36 -0500

    building a real maintained connection

--- c++_client/main.cpp
+++ c++_client/main.cpp
@@ -7,15 +7,39 @@
 
 using boost::asio::ip::tcp;
 
+static int count = 1;
+
 std::string make_daytime_string()
 {
   std::time_t now = std::time(0);
   return std::ctime(&now);
 }
 
+void transmit_data(tcp::socket& mySocket) 
+{
+  int messageCount = 0;
+  std::cout << "Please input number of messages to send (enter 0 to quit): \n";
+  std::cin >> messageCount;
+  for(int i = 0; i < messageCount; i++)
+  {
+    example::DataPacket dataPacket;
+    dataPacket.set_id(i+1);
+    dataPacket.add_payload()->set_timestamp(make_daytime_string());
+    dataPacket.add_payload()->set_timestamp(make_daytime_string());
+    dataPacket.add_payload()->set_timestamp(make_daytime_string());
+    dataPacket.add_payload()->set_timestamp(make_daytime_string());
+    dataPacket.add_payload()->set_timestamp(make_daytime_string());
+    dataPacket.add_payload()->set_timestamp(make_daytime_string());
+
+    boost::system::error_code ignored_error;
+    boost::asio::write(mySocket, boost::asio::buffer(dataPacket.SerializeAsString()), boost::asio::transfer_all(), ignored_error);
+  }
+
+  usleep(2000); // why do I need a usleep?  GUESS: packet is still processing asynchronously while the method is exiting  
+}
+
 int main(int argc, char* argv[])
 {
-  int count = 1;
   try
   {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
@@ -31,23 +55,12 @@ int main(int argc, char* argv[])
     tcp::resolver::query query(tcp::v4(), argv[1], argv[2]);
     tcp::resolver::iterator iterator = resolver.resolve(query);
 
-    tcp::socket s(io_service);
-    s.connect(*iterator);
+    tcp::socket mySocket(io_service);
+    mySocket.connect(*iterator);
 
-    example::DataPacket dataPacket;
-    dataPacket.set_id(count++);
-    dataPacket.add_payload()->set_timestamp(make_daytime_string());
-    dataPacket.add_payload()->set_timestamp(make_daytime_string());
-    dataPacket.add_payload()->set_timestamp(make_daytime_string());
-    dataPacket.add_payload()->set_timestamp(make_daytime_string());
-    dataPacket.add_payload()->set_timestamp(make_daytime_string());
-    dataPacket.add_payload()->set_timestamp(make_daytime_string());
+    transmit_data(mySocket);
 
-    boost::system::error_code ignored_error;
-    boost::asio::write(s, boost::asio::buffer(dataPacket.SerializeAsString()), boost::asio::transfer_all(), ignored_error);
-
-    usleep(2000); // why do I need a usleep?
-    s.close();
+    mySocket.close();
   }
   catch (std::exception& e)
   {
