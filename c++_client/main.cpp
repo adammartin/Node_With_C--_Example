51b54fcf077689ba2cf529f86bb207334234ba5c
adammartin <ebondark.od@gmail.com>
2013-08-21 06:40:40 -0500

    Initializing my sample projectg

---
+++ c++_client/main.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <iostream>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+std::string make_daytime_string()
+{
+  std::time_t now = std::time(0);
+  return std::ctime(&now);
+}
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 3)
+    {
+      std::cerr << "Usage: <App_Name_Here> <host> <port>\n";
+      return 1;
+    }
+
+    boost::asio::io_service io_service;
+
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query(tcp::v4(), argv[1], argv[2]);
+    tcp::resolver::iterator iterator = resolver.resolve(query);
+
+    tcp::socket s(io_service);
+    s.connect(*iterator);
+
+    std::string data = "Insert Protobuf data here\n\r";
+
+    boost::system::error_code ignored_error;
+    boost::asio::write(s, boost::asio::buffer(make_daytime_string()), boost::asio::transfer_all(), ignored_error);
+
+    usleep(2000); // why do I need a usleep?
+    s.close();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
