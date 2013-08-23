a52fba4e5b810dcf6162b0bf9405ba81a6115f42
adammartin <ebondark.od@gmail.com>
2013-08-23 18:08:11 -0500

    Creating a web socket client

--- node_server/tcp_server.js
+++ node_server/tcp_server.js
@@ -1,9 +1,66 @@
+#!/usr/bin/env node
 var net = require('net');
 var fs = require('fs');
 var proto = require('node-protobuf').Protobuf;
-var serverPort = 8124;
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var tcpServerPort = 8124;
+var httpServerPort = 8080;
 
-var server = net.createServer(function(socket) {
+/**** WebServer Connection Stuff ****/
+
+var myHttpServer = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+myHttpServer.listen(httpServerPort, function() {
+    console.log((new Date()) + ' Server is listening on port ' + httpServerPort);
+});
+
+wsServer = new WebSocketServer({
+    httpServer: myHttpServer,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
+
+/**** TCP SERVER CONNECTION STUFF ****/
+
+var tcpServer = net.createServer(function(socket) {
   var lastMessageId = 0;
   var lostMessageCount = 0;
   var protobuf = new proto(fs.readFileSync("../c++_client/message.desc"));
@@ -33,6 +90,6 @@ var server = net.createServer(function(socket) {
   });
 });
 
-server.listen(serverPort, function() {
+tcpServer.listen(tcpServerPort, function() {
   console.log('server bound');
 });
